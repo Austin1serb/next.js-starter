76076485a5c73af904c1e5af2edaea280189bed7
Austin1serb <serbaustin@gmail.com>
2025-08-23 14:41:55 -0700

    fix next.config

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {}
+
+export default nextConfig
+
+// MDX
+// const withMDX = createMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//   },
+// })
+
+// export default withMDX(nextConfig)
